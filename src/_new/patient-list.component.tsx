38ca3eb5796e37d936d8561010c7902d56046c0d
Maximilian Irlinger <max@maxirlinger.at>
2025-11-27 20:36:39 +0100

    use empty state in patient list

--- src/_new/patient-list.component.tsx
+++ src/_new/patient-list.component.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Patient } from "@openmrs/esm-framework/src";
 import { ClickableTile } from "@carbon/react";
+import { EmptyState } from "./empty-state.component";
+import { UserMultiple } from "@carbon/react/icons";
 import styles from './patient-list.scss';
 
 type Props = {
@@ -25,14 +27,22 @@ export const PatientList: React.FC<Props> = ({ patients, selectedPatient, onPati
             <div className={styles.patientList}>
                 <div className={styles.patientListHeader}>Patients</div>
                 <div className={styles.patientItems}>
-                {patients.map(patient => (
-                    <PatientTile 
-                        key={patient.uuid}
-                        patient={patient}
-                        isSelected={selectedPatient?.uuid === patient.uuid}
-                        onSelect={() => onPatientSelect?.(patient)}
+                {patients.length > 0 ? (
+                    patients.map(patient => (
+                        <PatientTile 
+                            key={patient.uuid}
+                            patient={patient}
+                            isSelected={selectedPatient?.uuid === patient.uuid}
+                            onSelect={() => onPatientSelect?.(patient)}
+                        />
+                    ))
+                ) : (
+                    <EmptyState 
+                        icon={UserMultiple}
+                        title="No patients in queue"
+                        description="There are currently no patients waiting in this stage"
                     />
-                ))}
+                )}
                 </div>
             </div>
         </aside>
