45a822d8176c1b7c41f3fbc1be86503e2e7e33a9
Maximilian Irlinger <max@maxirlinger.at>
2025-11-27 20:17:42 +0100

    move patient list out of tab

--- src/_new/workflows/generic-workflow.tsx
+++ src/_new/workflows/generic-workflow.tsx
@@ -7,6 +7,7 @@ import { MoveButton } from "../move-button";
 import { useQueueEntries } from "../hooks/use-queue-entries";
 import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@carbon/react";
 import { AlignBoxTopLeft } from "@carbon/react/icons";
+import styles from "./generic-workflow.scss";
 
 type Props = {
     stage: Stage;
@@ -49,43 +50,49 @@ export const GenericWorkflow = (props: Props) => {
     };
     
     return (
-        <Tabs>
-            <TabList contained>
-                <Tab renderIcon={AlignBoxTopLeft}>Form</Tab>
-                <Tab renderIcon={CalendarHeatMapIcon}>Visits</Tab>
-                <Tab renderIcon={ConditionsIcon}>Conditions</Tab>
-                <Tab renderIcon={ProgramsIcon}>Therapies</Tab>
-            </TabList>
-            <TabPanels>
-                <TabPanel>
-                    <div style={{ display: 'flex', gap: '20px' }}>
-                    <PatientList patients={queueEntries.map(entry => entry.patient)} selectedPatient={selectedPatient} onPatientSelect={(patient) => setSelectedPatient(patient)} />
+        <div className={styles.workflowWrapper}>
+            <div className={styles.patientListContainer}>
+                <PatientList 
+                    patients={queueEntries.map(entry => entry.patient)} 
+                    selectedPatient={selectedPatient} 
+                    onPatientSelect={(patient) => setSelectedPatient(patient)} 
+                />
+            </div>
 
-                    <div style={{ flex: 2 }}>
-                        {selectedPatient ? (
-                            <div>
-                                <h3>Patient Form</h3>
-                                <FormEngine 
-                                    formJson={dummySchema} 
-                                    patientUUID={selectedPatient.uuid}
-                                    visit={undefined}
-                                    formSessionIntent="enter"
-                                />
-                                <MoveButton queueEntry={queueEntries.find(entry => entry.patient.uuid === selectedPatient?.uuid)!} nextStage={props.nextStage} />
-                            </div>
-                        ) : (
-                            <div>
-                                <h3>Select a patient</h3>
-                                <p>Choose a patient from the queue to view and fill out their form</p>
-                            </div>
-                        )}
-                    </div>
-                </div>
-                </TabPanel>
-                <TabPanel>Coming soon...</TabPanel>
-                <TabPanel>Coming soon...</TabPanel>
-                <TabPanel>Coming soon...</TabPanel>
-            </TabPanels>
-        </Tabs>
+            <div className={styles.tabsContainer}>
+                <Tabs>
+                    <TabList contained>
+                        <Tab renderIcon={AlignBoxTopLeft}>Form</Tab>
+                        <Tab renderIcon={CalendarHeatMapIcon}>Visits</Tab>
+                        <Tab renderIcon={ConditionsIcon}>Conditions</Tab>
+                        <Tab renderIcon={ProgramsIcon}>Therapies</Tab>
+                    </TabList>
+                    <TabPanels>
+                        <TabPanel>
+                            {selectedPatient ? (
+                                <div>
+                                    <h3>Patient Form</h3>
+                                    <FormEngine 
+                                        formJson={dummySchema} 
+                                        patientUUID={selectedPatient.uuid}
+                                        visit={undefined}
+                                        formSessionIntent="enter"
+                                    />
+                                    <MoveButton queueEntry={queueEntries.find(entry => entry.patient.uuid === selectedPatient?.uuid)!} nextStage={props.nextStage} />
+                                </div>
+                            ) : (
+                                <div>
+                                    <h3>Select a patient</h3>
+                                    <p>Choose a patient from the queue to view and fill out their form</p>
+                                </div>
+                            )}
+                        </TabPanel>
+                        <TabPanel>Coming soon...</TabPanel>
+                        <TabPanel>Coming soon...</TabPanel>
+                        <TabPanel>Coming soon...</TabPanel>
+                    </TabPanels>
+                </Tabs>
+            </div>
+        </div>
     );
 }
